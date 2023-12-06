d7ff34afc61d5bf2253c90b815eeee17770a8bfa
thiyangt <thiyanga.talagala@monash.edu>
2023-12-06 07:11:30 +0530

    updated

---
+++ template.R
@@ -0,0 +1,28 @@
+```{r, message=FALSE, warning=FALSE,echo=FALSE}
+library(tidyverse)
+library(drone)
+library(patchwork)
+data("worldbankdata")
+```
+
+## geom_area{#area}
+
+### Package
+
+ggplot2 [@ggplot2]
+
+### Description
+
+### Understandable aesthetics
+
+`alpha`, `colour`, `linetype`, `linewidth`
+
+### The statistical transformation to use on the data for this layer 
+
+`stat_align`
+
+### See also
+
+[geom_line](#line), [geom_ribbon](#ribbon)
+  
+  ### Example
