1cad3a2cb1d32a50cb8b1615638fbeb8e2b70941
thiyangt <tstalagala@gmail.com>
2026-07-14 14:58:09 +0530

    updated

---
+++ aes.R
@@ -0,0 +1,7 @@
+**Aesthetics**
+  
+  | Type     | Aesthetics   |
+  | -------- | ------------ |
+  | Required | `x`, `y` |
+  | Other supported aesthetics | `arrow,type`, `arrow.angle`, `arrow.length`, `arrow.ends`, `arrow.type`|
+  
